51f5a39be5fe88c17f569d4e1136b18acd110b8d
seif basel <seifbasel950@gmail.com>
2026-02-19 17:41:12 +0200

    v-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AssetTracker from "@/sections/AssetTracker";
 
 export default function Home() {
   return (
-    <div className="bg-zinc-50 border-2 border-zinc-50 font-sans dark:bg-black">
+    <div className="font-sans dark:bg-black">
     <AssetTracker/>
     </div>
   );
